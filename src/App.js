22e4355f88cd8932917a04c9990d6da3d9f9970e
Gabrieldrc <gabriel.drc25@gmail.com>
2020-11-10 18:55:58 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <div className="nav_content">
+          <ul>
+            <a href="" class="nav_link" key="nav_link_aboutme">About Me</a>
+            <a href="" class="nav_link" key="nav_link_fontend">Front-End</a>
+            <a href="" class="nav_link" key="nav_link_backend">Back-End</a>
+          </ul>
+        </div>
+      </nav>
+      <div className="body_content">
+        <div className="left_menu" id="left_menu">
+          <a href="" class="nav_link" key="nav_link_aboutme">About Me</a>
+          <a href="" class="nav_link" key="nav_link_fontend">Front-End</a>
+          <a href="" class="nav_link" key="nav_link_backend">Back-End</a>
+        </div>
+        <div className="content_box">
+          <div>asfgadfgsdfg</div>
+          <div className="screens_container">
+            <div className="screen code_screen">
+              <div className="code_line">
+                <span className="number_line">1</span>  <span className="purple_word">import</span> <span className="blue_word">React</span> <span className="purple_word">from</span> <span className="path">'react'</span>
+              </div>
+            </div>
+            <div className="screen render_screen">
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer>
+
+      </footer>
     </div>
   );
 }
