f1d1f1e2c74872bd4725e286f0498c12b1d0ccbb
Gabrieldrc <gabriel.drc25@gmail.com>
2020-11-13 17:23:12 -0300

    TeminalPath component created

---
+++ src/components/TerminalPath/TerminalPath.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TerminalPath() {
+  return(
+    <di>
+      <span style={{color: "#0EE109"}}>@GABRIELRODRIGUEZ</span><span style={{color: "#2FDAFF"}}></span>
+    </di>
+  );
+}
+
+export default TerminalPath;
