67cbd14bf37559348ea9f2616165f91fa70f04bc
Gabrieldrc <gabriel.drc25@gmail.com>
2020-11-24 13:56:36 -0300

    .

--- src/components/ContentBox/ContentBox.js
+++ src/components/ContentBox/ContentBox.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './ContentBox.css';
 
 function ContentBox(props) {
+  const [visibility, setVisibility] = useState("hidden");
+  let contentVisibilityStyle = {
+    display: "block",
+    visibility: visibility,
+  };
+  useEffect(() => {  
+    setTimeout(() => { setVisibility("visible") }, 1500);
+  },[])
   return(
     <div className="content_box">
-      {props.children}
+      <div style={contentVisibilityStyle}>
+        {props.children}
+      </div>
     </div>
   );
 }
--- src/components/SkillsPage/SkillsPage.js
+++ src/components/SkillsPage/SkillsPage.js
@@ -17,6 +17,7 @@ function SkillsPage() {
           {/* <Title>Skills</Title> */}
           <p>Half-giant jinxes peg-leg gillywater broken glasses large black dog Great Hall. Nearly-Headless Nick now string them together, and answer me this, which creature would you be unwilling to kiss? Poltergeist sticking charm, troll umbrella stand flying cars golden locket Lily Potter. Pumpkin juice Trevor wave your wand out glass orbs, a Grim knitted hats. Stan Shunpike doe patronus, suck his soul Muggle-Born large order of drills the trace. Bred in captivity fell through the veil, quaffle blue flame ickle diddykins Aragog. Yer a wizard, Harry Doxycide the woes of Mrs. Weasley Goblet of Fire.</p>
         </ContentBox>
+        <a href="#" className="scroll_section"><span></span>Scroll downsdfgsd</a>
       </section>
       <section>
         <ContentBox>
