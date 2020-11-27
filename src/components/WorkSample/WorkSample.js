cec2965a4b420503bb8f44c0c6980dd5b7aa3479
Gabrieldrc <gabriel.drc25@gmail.com>
2020-11-27 16:45:44 -0300

    adding a video

--- src/components/WorkSample/WorkSample.js
+++ src/components/WorkSample/WorkSample.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './WorkSample.css';
+import htmlCssJsVideo from '../../videos/htmlCssJsWorking.mp4';
 
 function WorkSample() {
   return(
     <div className="body_content">
-      <section>
-        <div style={{color: "white", margin: "auto"}}>hola</div>
+      <section id="frontend_resume_box">
+        <video width="320" height="240" autoPlay loop muted>
+          <source src={htmlCssJsVideo} type="video/mp4"></source>
+        </video>
       </section>
     </div>
   );
